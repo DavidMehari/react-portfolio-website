071439e34cf6fef53a3e530adbf5d98dba3e675d
Mehari David <david.mehari@gmail.com>
2022-03-25 22:55:18 +0100

    about me done

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,58 @@
-import React from 'react'
+import React from 'react';
 import './about.css';
+import ME from '../../assets/harold-about.png';
+import { FaAward } from 'react-icons/fa';
+import { FiUsers } from 'react-icons/fi';
+import { VscFolderLibrary } from 'react-icons/vsc';
 
 const About = () => {
   return (
-    <section id='about'>About</section>
-  )
-}
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
 
-export default About
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img
+              src="https://hungarytoday.hu/wp-content/uploads/2021/04/hide-the-pain-harold.jpg"
+              alt="About Image"
+            />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>300+ Worldwide</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis,
+            reiciendis aspernatur? Optio sit quisquam numquam aliquid iure,
+            possimus exercitationem neque corrupti ipsa quod blanditiis ad, quae
+            cupiditate dignissimos impedit doloribus.
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
