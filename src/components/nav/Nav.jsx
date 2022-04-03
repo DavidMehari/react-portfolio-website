39c4dba8741283a6bc68762cb65fa388612205c8
Mehari David <david.mehari@gmail.com>
2022-04-03 20:50:11 +0200

    nav changed to scrollspy for active change on scroll as well

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,28 +3,22 @@ import './nav.css';
 import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai';
 import { BiBook, BiMessageSquareDetail } from 'react-icons/bi';
 import { RiServiceLine } from 'react-icons/ri';
+import Scrollspy from 'react-scrollspy';
 
 const Nav = () => {
 
-  const [activeNav, setActiveNav] = useState('#')
-
   return (
     <nav>
-      <a href="#home" onClick={() => setActiveNav('#home')} className={activeNav === '#home' ? 'active' : ''}>
-        <AiOutlineHome />
-      </a>
-      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}>
-        <AiOutlineUser />
-      </a>
-      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}>
-        <BiBook />
-      </a>
-      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}>
-        <RiServiceLine />
-      </a>
-      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}>
-        <BiMessageSquareDetail />
-      </a>
+      <Scrollspy
+        items={ ['home', 'about', 'experience', 'services', 'contact'] }
+        currentClassName="active"
+      >
+        <a href="#home"><AiOutlineHome /></a>
+        <a href="#about"><AiOutlineUser /></a>
+        <a href="#experience"><BiBook /></a>
+        <a href="#services"><RiServiceLine /></a>
+        <a href="#contact"><BiMessageSquareDetail /></a>
+      </Scrollspy>
     </nav>
   );
 };
